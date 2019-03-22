665692cf38fb7e0d12fff931e50f72b6dd1632bb
clem9281 <clem9281@gmail.com>
2019-03-22 08:36:54 -0700

    boilerplated

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 // play this: https://www.youtube.com/watch?v=d-diB65scQU
 
 // code away!
+const express = require("express");
+const helmet = require("helmet");
+
+const app = express();
+
+app.use(express.json());
+app.use(helmet());
+
+app.get("/", (req, res) =>
+  res.send("<h1>Sprint Challenge: Express and Node.js</h1>")
+);
+
+const port = process.env.PORT || 6500;
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
